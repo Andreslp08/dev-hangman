49277486814a6d6ba4ed23521364672f770ccc1b
Andrés López <andreslp08@hotmail.com>
2022-05-21 15:55:32 -0500

    actualizado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import { Icon } from '@iconify/react';
+
+import { useState } from 'react';
 import './styles/styles.scss'
 
+const ESTADOS = {
+  partidaIniciada: 'PARTIDA_INICIADA',
+  partidaGanada: 'PARTIDA_GANADA',
+  partidaFinalizada: 'PARTIDA_FINALIZADA',
+  lobby: 'LOBBY'
+}
+
 function App() {
+
+  const [gameState, setGameState] = useState({
+    puntaje: 0,
+    intentos: 0,
+    categoria: '',
+    palabra: '',
+    estado: [ESTADOS.lobby]
+  })
+
+ 
   return (
     <div className="App">
+  
       <h1 className='titulo'>Dev Hangman</h1>
-      <button className='main-box primary-button-bordered border-medium '>Hola</button>
+    
     </div>
   );
 }
